32ff9af0ee211a819fe4cd63f840c31152f4c037
hastenfernandes <hastenf@gmail.com>
2014-05-02 20:19:26 +0530

    scraping 2nd page done

--- hello.js
+++ hello.js
@@ -1,4 +1,5 @@
-TedLeader = new Meteor.Collection("tedleader");
+TedSpeakers = new Meteor.Collection("tedspeakers");
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to hello.";
@@ -17,7 +18,87 @@ if (Meteor.isServer) {
     Meteor.startup(function () {
         // scrapTed();
     });
+
+    var result,moreResult;
+
     
+    // scrapTed();
+    getMoreDetails();
 
 
 }
+
+function scrapTed(){
+      var $ = null,result = null;
+      var url = "http://www.ted.com/speakers";
+      result = Meteor.http.get(url);
+      $ = cheerio.load(result.content);
+      var designation = $(".col");
+      var currentDiv = null;
+      //currentDiv = cheerio.load(designation[0])
+      // console.log(currentDiv);
+      // console.log(designation.length)
+      //var divProd = $('div.prod');
+      var myArray = [];
+      var currentJson = {};
+      var moredetails,imgurl,name,expert;
+      for(var i=0,il=designation.length;i<il;i++){
+          currentDiv = designation[i];
+          //console.log(designation[i].children[0].data)
+          // currentJson = {
+          //         moredetails :  "http://www.ted.com"+$(currentDiv).find('a').attr('href'),
+          //         imgurl : $(currentDiv).find('.thumb__tugger img').attr('src'),
+          //         name : $(currentDiv).find('.media__message h4').text(),
+          //         expert : $(currentDiv).find('.media__message .p4').text()
+          //     };
+          // myArray.push(currentJson);
+          moredetails = "http://www.ted.com"+$(currentDiv).find('a').attr('href');
+          imgurl = $(currentDiv).find('.thumb__tugger img').attr('src');
+          name = $(currentDiv).find('.media__message h4').text();
+          expert = $(currentDiv).find('.media__message .p4').text();
+          var currentcursor= TedSpeakers.findOne({"moredetails":moredetails});
+          if(currentcursor){
+            TedSpeakers.update({"moredetails":moredetails},{$set : {"imgurl":imgurl,"name":name,"expert":expert}});
+          }else{
+            var Follow = {"moredetails": moredetails,"imgurl":imgurl,"name": name,"expert": expert};
+            TedSpeakers.insert(Follow);
+          }
+      }
+      //console.log(moredetails);
+    }
+    function getMoreDetails(){
+      var $ = null,moreResult = null;
+      var link1,link2,link3,profileintro,listen,say
+      var activeurl = [];
+      //console.log("first")
+      TedSpeakers.find({}).forEach(function(data){
+          activeurl.push(data.moredetails);
+          //console.log(data.moredetails.length)
+      });
+      //console.log("second")
+      for(var i=0,il=activeurl.length-1;i<il;i++){
+          var url = activeurl[i];
+          if(url && (!url.match("undefined"))&&url.length>27){
+            moreResult = Meteor.http.get(url);
+            $ = cheerio.load(moreResult.content);
+
+            var a = $('.profile-header__links__inner a');
+            for(var j=0,jl=a.length;j<jl;j++){
+                //console.log($(a[j]).attr('href'));
+                link1 = $(a[0]).attr('href');
+                link2 = $(a[1]).attr('href');
+                link3 = $(a[2]).attr('href');
+            }
+            profileintro =$('.profile-intro').text();
+            //listen = $('.section section--minor p');
+            for(var k=0,kl=listen.length;k<kl;k++){
+                console.log($(listen[k]).text());
+            }
+            say =$('bloquote p').text();
+            // console.log(website);
+            // console.log(listen);
+          }
+          
+
+      }
+    }
--- packages/Evolve/server.js
+++ packages/Evolve/server.js
@@ -2,29 +2,3 @@ Future = Npm.require('fibers/future');
 jquery = Npm.require("jquery");
 scrap = Future.wrap(Npm.require('scrap'));
 cheerio = Npm.require("cheerio");
-var result;
-    function scrapTed(){
-        var $ = null,result = null;
-        var url = "http://www.ted.com/speakers";
-
-        result = Meteor.http.get(url);
-        $ = cheerio.load(result.content);
-        var designation = $(".col");
-        var currentDiv = null;
-        //currentDiv = cheerio.load(designation[0])
-        console.log(currentDiv);
-        console.log(designation.length)
-        //var divProd = $('div.prod');
-        var myArray = [];
-        //return;
-        for(var i=0,il=designation.length;i<il;i++){
-            currentDiv = designation[i];
-            // currentDiv.
-            //console.log(designation[i].children[0].data)
-            console.log($(currentDiv).find('.thumb__tugger img').attr('src'));
-            console.log($(currentDiv).find('.media__message h4').text());
-            console.log($(currentDiv).find('.media__message .p4').text());
-        }
-    }
-
-scrapTed();
